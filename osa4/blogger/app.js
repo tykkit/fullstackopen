2176d01c1efd3411a69cfbc9840f0a27d44c1197
Tyko Tamminen <tyko.tamminen@outlook.com>
2025-01-16 22:57:09 +0200

    part 4a exercises

---
+++ osa4/blogger/app.js
@@ -0,0 +1,25 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+mongoose.connect(config.MONGODB_URI)
+    .then(() => {
+        logger.info('Connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.error('error connecting: ', error.message)
+    })
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.errorHandler)
+
+module.exports = app
